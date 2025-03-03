a09319541579534ce9304e47e670200817946f47
tora180405 <toradigda@gmail.com>
2025-03-03 09:32:35 +0700

    jb4-prak2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,9 +28,7 @@ class UserController extends Controller
 
         // $user = UserModel::all();
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
