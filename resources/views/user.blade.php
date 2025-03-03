d44c37bf85fd39d62a26af5afd950b278916f4e7
tora180405 <toradigda@gmail.com>
2025-03-03 10:45:52 +0700

    jb4-prak2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,25 +8,24 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="http://localhost/PWL_POS/public/user/tambah">+ Tambah User</a>
     <table border="1" cellpading="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Aksi</th>
         </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
-        {{-- <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr> --}}
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="http://localhost/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="http://localhost/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+            </tr>
+        @endforeach
     </table>
 </body>
 </html>
